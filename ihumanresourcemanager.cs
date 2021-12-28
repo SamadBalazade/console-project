b9382fc0df9ccb8291626d098837f93cf6c9128c
SamadBalazade <94452675+SamadBalazade@users.noreply.github.com>
2021-12-28 16:44:04 +0400

    first commit

---
+++ ihumanresourcemanager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleproject
+{
+    
+ 
+        interface IHumanResourceManager
+        {
+            public Department[] Departments { get; }
+            void AddDepartment(string name, int workerlimit, double salarylimit);
+
+            void EditDepartments(string name, string newname);
+            void AddEmployee(string departmentname, string fullname, string position, double salary);
+            void RemoveEmployee(string no, string departmentname);
+            void EditEmployee(string departmentname, string no, string position, double salary);
+        }
+    }
+
+
